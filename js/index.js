c0027dc0e9cf293a207ea0dfc1939e13279afd5c
Małgosia Krzemińska <malgosia.krzeminska@yahoo.pl>
2017-03-09 10:33:28 +0100

    First commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+var a = 2;
+var b = 3;
+    value = (a*a) + (2 * a * b) - (b*b)
+console.log('Wynik działania wyniosi '+ value);
+if (value > 0 ) {
+  console.log('Wynik działania jest dodatni')
+}
+else if (value < 0) {
+  console.log('Wynik działania jest ujemny')
+}
+else {
+  console.log('Wynik działania jest równy 0')
+}
